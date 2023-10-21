82c1ee7a0f20a4a441400042882094876ca348f7
ReturnAlt10 <112390553+ReturnAlt10@users.noreply.github.com>
2023-10-21 13:18:13 +0100

    Added Code Files

---
+++ main.js
@@ -0,0 +1,51 @@
+video = "";
+status1 = "";
+objects = [];
+
+
+function preload() {
+    
+}
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("ModelLoaded!")
+    status1 = true;
+    objectDetector.detect(video, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw() {
+    image(video, 0, 0, 380, 380);
+
+        if(status1 !="")    
+        {
+            for (var i = 0; i < objects.length; i++) {
+                document.getElementById("status").innerHTML = "Status : Object Detected";
+                document.getElementById("num_objects").innerHTML = "Number of objects detected: "+objects.length;
+
+                fill("#FF0000");
+                percent = floor(objects[i].confidence * 100);
+                text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y);
+                noFill();
+                stroke("#FF0000");
+                rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            }
+        }
+}
